a873d72dd329d65a8e1d6272793027a8234ebc56
ha123g <2972515097@qq.com>
2026-08-30 17:52:04 +0800

    Initial project

---
+++ src/main/java/com/example/home_service_backend/HomeServiceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.home_service_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeServiceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeServiceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/home_service_backend/HomeServiceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.home_service_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeServiceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
